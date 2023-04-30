e9b0ff30f1b2f78f2d6050e834f21e53683f99f9
hucancode <hucancode@gmail.com>
2023-05-01 01:29:26 +0900

    fix build script

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,3 +4,4 @@ source "$HOME/.cargo/env"
 echo "Installing wasm-pack..."
 # Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+wasm-pack build ./poker-solver --target web
