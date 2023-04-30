eedcd5dd388cac2e4836b62e0b5f070d69c4456f
hucancode <hucancode@gmail.com>
2023-05-01 01:23:54 +0900

    add toolchain installer

---
+++ install-toolchain.sh
@@ -0,0 +1,6 @@
+echo "Installing Rustup..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
+echo "Installing wasm-pack..."
+# Install wasm-pack
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
