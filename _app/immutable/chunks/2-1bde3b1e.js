0e8e8bedd05f45a352cedec55f7f42fe82f1d2c2
hucancode <hucancode@gmail.com>
2023-06-11 09:12:46 +0900

    Updates

---
+++ _app/immutable/chunks/2-1bde3b1e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-baa74e2d.js";export{t as component};
--- _app/immutable/chunks/2-ff0d6611.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-82e67345.js";export{t as component};
